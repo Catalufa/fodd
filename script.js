84f7237eb5cdaf528b24065df870fdeab7624da1
Catalufa <79006629+Catalufa@users.noreply.github.com>
2024-03-21 10:34:17 +0000

    Updates

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ document.addEventListener("mousemove", evt => {
     let mouse = event.clientX - center
     photo.style.transform = `rotate(${5/document.body.clientWidth*mouse}deg)`
 })
+
+let navSpans = nav.querySelectorAll("span")
+navSpans.forEach(span => {
+  span.addEventListener("click", (evt) => {
+    let newHref = "/"
+    const targetText = (evt.target.innerText).toLowerCase()
+    if (targetText != "home") {
+      newHref = targetText
+    }
+    // window.location.href = newHref
+    Swal.fire("Whoops!", "We're still working on our website. That page is coming soon!", "info")
+  })
+})
+
+let buttons = document.querySelectorAll("button:not([data-save])")
+buttons.forEach(btn => {
+  btn.addEventListener("click", evt => {
+    Swal.fire("Whoops!", "We're still working on our website. That page is coming soon!", "info")
+  })
+})
